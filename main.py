9d857b9befa9328e70ec487462f7bd7ca725a6e6
Jasmine Li <jasmineli106@gmail.com>
2019-12-09 10:28:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,25 +26,22 @@ degree=0
 
 #Moving arm to initial position
 hand_motor.run_target(500,90)
+brick.light(Color.RED)
 
 #Infinite loop
 while True:
     distance = spider_eye.distance(False)
-    brick.light(Color.RED)
     #If something approaches sensor
     if distance <= 2000:
         #Arm moves
-        print("Turn 1")
         hand_motor.run_target(500,-90)
-        wait(1000)
-        print("Turn 2")
+        wait(500)
         hand_motor.run_target(500,90)
         wait(500) 
         #Nose spins
         nose_motor.run_target(500,360)
         brick.sound.file(SoundFile.LAUGHING_1)
         nose_motor.run_target(500,0)
-        wait(1500)
     
     brick.light(Color.GREEN)
 
@@ -55,8 +52,11 @@ while True:
             #Dispenser rotates, drops candy
             degree = degree + 45
             dispenser_motor.run_target(500,degree)
-            i = 50
+            while button.pressed():
+                wait(10)
+            break
         else:
             wait(10)
-        i += 1
+            i += 10
+    print(i)
     brick.light(Color.RED)
